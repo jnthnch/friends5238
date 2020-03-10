1bac1b80accc3041c503f761088756260828b7af
jnthnch <jchu127@gmail.com>
2020-03-09 17:57:13 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+app.listen(app.get('port'), () => {
+  console.log(`Server started: http://localhost:${app.get('port')}/`);
+});
---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className='header-1'>Hello from App.js</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
