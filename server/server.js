10dd4887a8300a98c950eda35d1b231ade0d0e2d
jnthnch <jchu127@gmail.com>
2020-03-10 15:21:48 -0700

    Fix cannot GET/URL error on refresh with React Router, added app.,get function inside server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,14 @@ app.set('port', process.env.PORT || 3000);
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 app.listen(app.get('port'), () => {
   console.log(`Server started: http://localhost:${app.get('port')}/`);
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import "@babel/polyfill";
 import '../../public/style.css';
+import { Route, Link } from "react-router-dom";
 
 import SignInPage from './SignInPage'
+
+const Eat = (props) => (
+  <div>
+    <h1>NOM NOM NOM</h1>
+    <pre>{JSON.stringify(props, null, 4)}</pre>
+    <iframe
+      src="https://giphy.com/embed/VvQvOFqPjZLi"
+      width="480"
+      height="480"
+    />
+    <p>So tasty!</p>
+  </div>
+);
+
+const Drink = () => (
+  <div>
+    <h1>SO REFRESHING</h1>
+    <iframe
+      src="https://giphy.com/embed/DbD6EnlEQmjTi"
+      width="480"
+      height="461"
+    />
+  </div>
+);
 
 class App extends React.Component {
   constructor(props) {
@@ -12,7 +37,20 @@ class App extends React.Component {
   render() {
     return (
       <div className='main'>
-        <SignInPage></SignInPage>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/eat">Eat</Link>
+          </li>
+          <li>
+            <Link to="/drink">Drink</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={SignInPage} />
+        <Route path="/eat" component={Eat} />
+        <Route path="/drink" component={Drink} />
       </div >
     );
   }
